9d9b99f0847377f5fb40cd8811fc2aa5005c8791
Loky31 <bensurv31@gmail.com>
2019-09-06 18:18:44 +0200

    Add files via upload

--- action-Telecommande-kodi.py
+++ action-Telecommande-kodi.py
@@ -70,11 +70,14 @@ def inject():
     tupel = build_tupel(kodi.get_shows(),'title')
     send['operations'][0][1]['shows'] = send['operations'][0][1]['shows']+tupel
     tupel = build_tupel(kodi.get_genre(),'title')
-    request= [
-        AddFromVanillaInjectionRequest(send)
-    ]
-    with Hermes(mqtt_options=mqtt_opts) as h:
-        h.request_injection(request)
+    operations.append(send)
+    update_request = {"operations":operations}
+    self.mqtt_client.publish('hermes/injection/perform', json.dumps(update_request))
+    #request= [
+    #    AddFromVanillaInjectionRequest(send)
+    #]
+    #with Hermes(mqtt_options=mqtt_opts) as h:
+    #    h.request_injection(request)
     #Hermes.publish("hermes/injection/perform",json.dumps(send))
     return
 
